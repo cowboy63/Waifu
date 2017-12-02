af3d2253c6b7befc73d2e04db990db2c7428e71f
Kevin Chen <ningchen227@gmail.com>
2017-12-02 11:20:39 -0600

    New stuff

--- scripts/content.js
+++ scripts/content.js
@@ -1,2 +1,13 @@
 // content.js
-alert("Hello from your Chrome extension!")
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "clicked_browser_action" ) {
+        var firstHref = $("a[href^='http']").eq(0).attr("href");
+  
+        console.log(firstHref);
+  
+        // This line is new!
+        chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+      }
+    }
+  );
