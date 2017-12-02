4eac94d11f1588d0326b90f31f676c3826c4e764
Kevin Chen <ningchen227@gmail.com>
2017-12-02 13:42:28 -0600

    Stretch

--- src/page_action/img.js
+++ src/page_action/img.js
@@ -3,7 +3,7 @@ function dispImg()
 	const NUM_IMG = 6;
 	var randNum = Math.floor((Math.random() * NUM_IMG) + 1);
 	var imag = 'waifu' + randNum + '.png';
-	var imgStr = '<img src="' + imag + '" alt = "">';
+	var imgStr = '<img src="' + imag + '" alt = "A proud waifu" style="width:200px;height:200px;">';
 	document.write(imgStr);
 }
 
