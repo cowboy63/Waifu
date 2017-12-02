758a91d5a97a7c990ca438e3dfe433ee25c25d01
Arami Guerra de la Llera <arami.g@outlook.com>
2017-12-02 14:35:46 -0600

    search ready

--- website/scripts/web.js
+++ website/scripts/web.js
@@ -1,4 +1,12 @@
-$(document).ready(function()
-{
-    
+$(document).ready(function () {
+    var sub = $("#sub");
+    var te = $("#text");
+   
+    var ff = "https://www.google.com/search?q=";
+
+    $(sub).click(function (e) {
+        ff+=te.val();
+        window.open(ff, '_self');
+    });
+  
 });
