3d6f3e05925dc3cd9df07955ca44fac6a257fd3b
Kevin Chen <ningchen227@gmail.com>
2017-12-02 14:26:21 -0600

    New waifu videos

--- src/page_action/img.js
+++ src/page_action/img.js
@@ -3,35 +3,49 @@ function dispImg()
 	const NUM_IMG = 6;
 	var randNum = Math.floor((Math.random() * NUM_IMG) + 1);
 	var waifuName;
+	document.write("<font size=\"2\">Your new waifu is: </font>");
+	document.write("<center>");
+	document.write("<h2><font size=\"6\"><font face=\"Comic Sans MS\">");
 	switch(randNum)
 	{
 		case 1:
 		waifuName = 'Honoka';
+		videoStr = 'tZobzXlIPQU';
 		break;
 		case 2:
 		waifuName = 'Megumi';
+		videoStr = '8azxn5ZeD2w';
 		break;
 		case 3:
 		waifuName = 'Tomoko';
+		videoStr = '2Tcrtty1z60';
 		break;
 		case 4:
 		waifuName = 'Haruhi';
+		videoStr = 'OYVlx3vnu8I';
 		break;
 		case 5:
 		waifuName = 'Chitose';
+		videoStr = 'vdWZKJ2Y8pU';
 		break;
 		case 6:
 		waifuName = 'Kyouko';
+		videoStr = '9gntk6woAjM';
 		break;
 		default:
 		waifuName = 'Wow, you\'ve got a special waifu!'
 		break;
 	}
-	document.write(waifuName);
 
+	document.write(waifuName);
+	document.write("</h2></font>");
+	document.write("<br>");
 	var imag = 'waifu' + randNum + '.png';
 	var imgStr = '<img src="' + imag + '" alt = "A proud waifu" style="width:200px;height:200px;">';
 	document.write(imgStr);
+	document.write("<br>");
+	document.write("<iframe width=\"560\" height=\"315\" src=\"https://youtube.com/embed/"+videoStr+"?&autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>")
+	document.write("</center>");
 }
 
 document.getElementById('print-image').onclick = dispImg;
