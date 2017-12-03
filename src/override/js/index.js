780af7c6d61ae266f990b76673baacc7f224f17b
Kevin Chen <ningchen227@gmail.com>
2017-12-02 19:16:58 -0600

    Revert "Changed index"
    This reverts commit 1033a1771e9439c72fd017d6f7e537c4e742b4c7.

---
+++ src/override/js/index.js
@@ -0,0 +1,12 @@
+var checked = $(".cbox");
+
+checked.click(function() {
+  if (checked.prop("checked")) {
+    $(".add").text("Hit Enter to Submit");
+  }
+
+  if (!checked.prop("checked")) {
+    $(".message").val("");
+    $(".add").text("Search");
+  }
+});
