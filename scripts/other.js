3855a201eb4f482ee9ae92e3e6e5c8787dc3ee10
sawyer hill <Sawyer@hillhome.com>
2017-12-02 21:13:05 -0600

    changed other

--- scripts/other.js
+++ scripts/other.js
@@ -1,1 +1,10 @@
-javascript:var imgs = document.getElementsByTagName("img");for(var i=0, l=imgs.length;i<l;i++){imgs[i].src = "waifu.png";}
+javascript:var imgs = document.getElementsByTagName("img");
+for(var i=0, l=imgs.length;i<l;i++){
+	var randNum = Math.floor((Math.random() * 150) + 1);
+	if(randNum==5)
+	imgs[i].src = "https://usercontent2.hubstatic.com/13014199_f520.jpg";
+	var randNum = Math.floor((Math.random() * 150) + 1);
+	if(randNum==5)
+	imgs[i].src = "https://i.redd.it/29xgi1yq66ex.jpg";
+	
+}
