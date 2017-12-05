086b2ee6134b04be8e288c1db9e212406ed3da99
SayerWer <SayerWer@users.noreply.github.com>
2017-12-05 16:46:59 -0600

    Update other.js

--- scripts/other.js
+++ scripts/other.js
@@ -1,10 +1,15 @@
 javascript:var imgs = document.getElementsByTagName("img");
 for(var i=0, l=imgs.length;i<l;i++){
-	var randNum = Math.floor((Math.random() * 150) + 1);
+	var randNum = Math.floor((Math.random() * 15500) + 1);
 	if(randNum==5)
 	imgs[i].src = "https://usercontent2.hubstatic.com/13014199_f520.jpg";
-	var randNum = Math.floor((Math.random() * 150) + 1);
+	var randNum = Math.floor((Math.random() * 15700) + 1);
 	if(randNum==5)
 	imgs[i].src = "https://i.redd.it/29xgi1yq66ex.jpg";
-	
+	var randNum = Math.floor((Math.random() * 20000) + 1);
+	if(randNum==5)
+	imgs[i].src = "http://img1.ak.crunchyroll.com/i/spire2/01b77374fb19f99d2f2225392ee19de21455588559_full.png";
+		var randNum = Math.floor((Math.random() * 25000) + 1);
+	if(randNum==5)
+	imgs[i].src = "https://pm1.narvii.com/6390/1fa1a52d3e4f22ad8f5118cc7927f3739d2d821d_hq.jpg";
 }
